7e3e9ba9b1edb4809470c0987cafdd35be786b9b
RKoopman <raoul.koopman@gmail.com>
2016-07-18 12:10:13 -0400

    validations custom validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
+
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250}
+  validates :summary, length: { maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbait_title
+
+
+
+  def clickbait_title
+    clickbaits = ["Won't Believe", "Secret", "Top", "Guess"]
+    if self.title
+      if clickbaits.none? { |click| self.title.include?(click) }
+        errors.add(:title, "Sorry, title is no bueno!")
+      end
+    end
+  end
 end
